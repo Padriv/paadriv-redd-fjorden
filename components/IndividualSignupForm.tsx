f1925aad0f48a0807c3b4be386ecd08ec63de4aa
aimeehong-bekk <aimee.hong@bekk.no>
2026-06-24 12:44:57 +0200

    add: lagd et eget skjema for de som ønsker å melde seg inn som privatperson med spørsmål og tekstinput

---
+++ components/IndividualSignupForm.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import { useState } from "react";
+
+
+export default function IndividualSignupForm() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [passion, setPassion] = useState("");
+
+    return (
+        <div className="relative p-8 rounded-2xl w-full flex flex-col gap-8">
+            <h2 className="text-xl font-bold">Bli med i Oppdrag: fjorden vår</h2>
+
+            <div className = "flex flex-col gap-2">
+                <label className="block text-sm font-medium mb-1">Navn:</label>
+                <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    placeholder="Ola Nordmann"
+                    className="w-full border border-zinc-300 rounded-lg px-3 py-2 text-sm outline-none focus:border-zinc-900"
+                />
+            </div>
+
+            <div className = "flex flex-col gap-2">
+                <label className="block text-sm font-medium mb-1 mt-3">E-post:</label>
+                <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="ola.nordmann@example.com"
+                    className="w-full border border-zinc-300 rounded-lg px-3 py-2 text-sm outline-none focus:border-zinc-900"
+                />
+            </div>
+
+            <div className = "flex flex-col gap-2">
+                <label className="block text-sm font-medium mb-1 mt-3">Tlf:</label>
+                <input
+                    type="tel"
+                    value={phoneNumber}
+                    onChange={(e) => setPhoneNumber(e.target.value)}
+                    placeholder="123 45 678"
+                    className="w-full border border-zinc-300 rounded-lg px-3 py-2 text-sm outline-none focus:border-zinc-900"
+                />
+            </div>
+
+            <div className = "flex flex-col gap-2">
+                <label className="block text-sm font-medium mb-1 mt-3">Er det noe spesielt du brenner for å bidra med?</label>
+                <input
+                    type="text"
+                    value={passion}
+                    onChange={(e) => setPassion(e.target.value)}
+                    placeholder="F.eks. rydde strender, undervise barn om miljø, etc."
+                    className="w-full border border-zinc-300 rounded-lg px-3 py-2 text-sm outline-none focus:border-zinc-900"
+                />
+            </div>
+        </div>
+    );
+}
