15571688a95100d3a9176ceebe411717990b090e
aimeehong-bekk <aimee.hong@bekk.no>
2026-06-24 15:23:43 +0200

    rename: endre navn på filen InterestButton til JoinSection for å samvare filnavn med innhold

--- components/JoinSection.tsx
+++ components/JoinSection.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function InterestButton() {
+export default function JoinSection() {
   return (
     <section
       id="meld-interesse-organisasjon"
